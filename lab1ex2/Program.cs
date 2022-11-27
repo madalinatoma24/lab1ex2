76820f92435d2cd2c34a45e164504345c33ed8fd
madalina.toma <madalina.toma@softem.ro>
2022-11-27 16:06:47 +0200

    Add project files.

---
+++ lab1ex2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace lab1ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Ex 2 Scrieti un program care va calcula media aritmetica a trei numere citite de la tastatura */
+            int firstNumber;
+            int secondNumber;
+            int thirdNumber;
+
+            Console.WriteLine("Introduceti primul numar: ");
+            firstNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduceti al doilea numar: ");
+            secondNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduceti al treilea numar: ");
+            thirdNumber = int.Parse(Console.ReadLine());
+
+            int media = (firstNumber + secondNumber + thirdNumber)/ 3;
+
+            Console.WriteLine("Media aritmetica este: " + media);
+        }
+    }
+}
